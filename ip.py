eb24b78a60655730b72862e7755fabc72b068e04
husky-parul <promptparul@gmail.com>
2017-04-21 05:29:50 -0400

    class ip for blacklisted and benign ips

--- ip.py
+++ ip.py
@@ -1,9 +1,13 @@
-class IP(object):
-    def __int__(self,ip,asn,asname,netblock):
-        self.ip=ip
-        self.asn=asn
-        self.asname=asname
-        self.netblock=netblock
+import datetime
+from datetime import date
+
+class IP:
+    def __init__(self, ip='', asn='',asname='',netblock='',firstseen=''):
+        self.ip = ip
+        self.asn = asn
+        self.asname = asname
+        self.netblock = netblock
+        self.firstseen = firstseen
 
     def getIp(self):
         return self.ip
@@ -15,4 +19,43 @@ class IP(object):
         return self.asname
 
     def getNetBlock(self):
-        return self.netblock
+        i= self.ip.rfind('.')
+        return self.ip[:8]+'.0'
+
+    def get_rank(self):
+        diff=self.getDateDiff('2017-03-21')
+        rank=1.0
+        for i in range(1,diff):
+            rank*=0.89
+        return rank
+
+    def getDateDiff(self,firstseen):
+        d = datetime.date.today().isoformat().split('-')
+        print 'd', d
+        y = d[0]
+        m = d[1]
+        days = d[2]
+        d = date(int(y), int(m), int(days))
+        print 'd: ', d
+
+        today = firstseen.split('-')
+        print today
+        ty = today[0]
+        tm = today[1]
+        tdays = today[2]
+        today = date(int(ty), int(tm), int(tdays))
+        print 'today', today
+
+        earlier = d - today
+
+        return earlier.days
+
+
+
+
+x = IP('10.10.10.10')
+print x.ip, x.asn
+print x.getNetBlock()
+print x.get_rank()
+
+
