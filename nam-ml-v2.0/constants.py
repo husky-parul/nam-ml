3da58289a08573b77a2ebb13c0eaa860afbc6521
husky-parul <promptparul@gmail.com>
2017-04-26 01:14:52 -0400

    final version 1.0

---
+++ nam-ml-v2.0/constants.py
@@ -0,0 +1,28 @@
+# files name
+BLACKLISTED_IP_SER='blacklisted_ip.ser'
+SHODAN_FULL_SER='22AprShodan_full.ser'
+CONFIGFILE_PROPERTIES='ConfigFile.properties'
+SHODAN_RAW='22AprShodan'
+SHODANDUMP='shodandump'
+MERGED_BENIGN='merged_benign'
+TOP_BLACKLISTED_IP_JSON='top_blacklisted_ip.json'
+BLACKLISTED_IP_SER='blacklisted_ip.ser'
+
+# properties constants
+
+SHODAN='shodan'
+SHODAN_URL='shodan.url'
+SHODAN_APIKEY='shodan.apikey'
+DSHIELD='dshield'
+URL_ATTACK='url.attack'
+URL_IP='url.ip'
+
+# SVM CROSS
+LINEAR='linear'
+NEG_LOG_LOSS='neg_log_loss'
+NEG_MEAN_SQUARED_ERROR='neg_mean_squared_error'
+THREE_D_PROJECTION='3d'
+
+# EXCEPTION MESSAGES
+TIME_OUT_ERR='Time out error occured: '
+ERROR='Error: %s'
